68430901edeffb6551bee10e1e95413cd0a8d80c
alex bishara <abishara@stanford.edu>
2016-07-16 21:02:14 -0700

    filters for chimeric reads and other bad local-asm links

--- athena/stages/assemble_meta_bins.py
+++ athena/stages/assemble_meta_bins.py
@@ -6,6 +6,8 @@ import glob
 import shutil
 from itertools import izip
 import random
+import numpy as np
+from bx.intervals.cluster import ClusterTree
 
 from ..assembler_tools.architect import architect
 from ..assembler_tools.architect import local_assembly
@@ -39,6 +41,8 @@ class AssembleMetaBinnedStep(StepChunk):
     
     for i, ((binid, bcode_set), (_, bcode_counts)) in \
         enumerate(izip(bins[:-1], bins2)):
+      if binid != 'bin.contig-100_5761':
+        continue
       yield AssembleMetaBinnedStep(options, binid, bcode_set, bcode_counts)
 
   def __init__(
@@ -96,7 +100,8 @@ class AssembleMetaBinnedStep(StepChunk):
       self.bcode_counts.items(),
     ))
 
-    local_asms = get_local_asms(
+    self.logger.log('determing local assemblies')
+    local_asms = self.get_local_asms(
       seed_ctgs,
       root_ctg, 
       self.bcode_set,
@@ -107,7 +112,6 @@ class AssembleMetaBinnedStep(StepChunk):
       os.path.join(asmrootdir_path, 'local-asm.p'),
       local_asms,
     )
-
     self.logger.log('found {} local assemblies'.format(len(local_asms)))
 
     rootfq_path = self.options.longranger_fqs_path
@@ -217,102 +221,230 @@ class AssembleMetaBinnedStep(StepChunk):
         "{}) but there's not much we can do so continuing".format(retcode))
 
 
-def get_local_asms(
-  seed_ctgs,
-  root_ctg, 
-  bcode_set,
-  bcode_counts,
-  bam_path,
-):
-  # FIXME need to fetch the barcoded reads from the neighbor contigs as well
-  def get_barcode(read):
-    filt_list = filter(lambda(k, v): k == 'BX', read.tags)
-    if filt_list == []: 
-      return None
-    else:
-      k, v = filt_list[0]
-      return v
+  def get_local_asms(
+    self,
+    seed_ctgs,
+    root_ctg, 
+    root_bcode_set,
+    bcode_counts,
+    bam_path,
+  ):
+    SMALL_CTG_SIZE = 1000
+    COV_BIN_SIZE = 100
+  
+    fhandle = pysam.Samfile(bam_path, 'rb')
+    ctg_size_map = util.get_fasta_sizes(self.options.ctgfasta_path)
 
-  fhandle = pysam.Samfile(bam_path, 'rb')
-  n_ctg_bcode_counts = defaultdict(Counter)
-  n_ctg_bcodes = defaultdict(set)
-  n_ctg_counts = Counter()
-  for read in fhandle.fetch(root_ctg):
-    if read.is_unmapped:
-      continue
-    # skip low quality links for now
-    if read.mapq < 10:
-      continue
-    bcode = get_barcode(read)
-    if bcode == None:
-      continue
-    p_ctg = fhandle.getrname(read.next_reference_id)    
-    n_ctg_counts[p_ctg] += 1
+    def get_barcode(read):
+      filt_list = filter(lambda(k, v): k == 'BX', read.tags)
+      if filt_list == []: 
+        return None
+      else:
+        k, v = filt_list[0]
+        return v
+  
+    def get_ctg_links(ctg):
+      bcodes = set()
+      bcode_counts = Counter()
+      link_ctg_counts = Counter()
+      link_ctg_qnames = defaultdict(set)
+      link_regions_map = defaultdict(lambda: ClusterTree(200, 1))
+      cov_bin_counts = Counter()
+      for i, read in enumerate(fhandle.fetch(ctg)):
+        if read.is_unmapped:
+          continue
+        # skip low quality links for now
+        if read.mapq < 10:
+          continue
+        bcode = get_barcode(read)
+        if bcode == None:
+          continue
+        bcodes.add(bcode)
+        bcode_counts[bcode] += 1
+        p_ctg = fhandle.getrname(read.next_reference_id)    
+        link_ctg_counts[p_ctg] += 1
+        link_ctg_qnames[p_ctg].add(read.qname)
+        link_regions_map[p_ctg].insert(read.pos, read.aend, i)
+  
+        # update coverage profile
+        bin_idx = read.pos / COV_BIN_SIZE
+        cov_bin_counts[bin_idx] += 1
+        
+      # filter for only links in which the paired end reads are clustered
+      # near each other
+      #print 'initial cand links', len(link_ctg_counts)
+      for p_ctg, regions in link_regions_map.items():
+        regions = link_regions_map[p_ctg]
+        max_hits = max(map(lambda(x): len(x[2]), regions.getregions()))
+        if max_hits < 3:
+          del link_ctg_counts[p_ctg]
+          del link_ctg_qnames[p_ctg]
+      #print 'post cand links', len(link_ctg_counts)
 
-  n_cand_ctgs = filter(
-    lambda(c): (c != root_ctg and n_ctg_counts[c] >= 3),
-    n_ctg_counts,
-  )
-  for n_ctg in n_cand_ctgs:
-    for read in fhandle.fetch(n_ctg):
-      if read.is_unmapped:
-        continue
-      # skip low quality links for now
-      if read.mapq < 10:
-        continue
-      bcode = get_barcode(read)
-      if bcode == None:
-        continue
-      n_ctg_bcodes[n_ctg].add(bcode)
-      n_ctg_bcode_counts[n_ctg][bcode] += 1
+      return (
+        bcodes,
+        bcode_counts,
+        link_ctg_counts,
+        link_ctg_qnames,
+        cov_bin_counts,
+      )
 
+    def is_whack_coverage(cov_bin_counts):
+      max_cov = max(cov_bin_counts.values())
+      med_cov = np.median(cov_bin_counts.values())
+      return max_cov > 20 * med_cov
 
-  # NOTE consider only doing local asms with neighbor if the neighbor is a
-  # large enough seed contig
-  n_ctgs = filter(
-    lambda(c): (
-      c != root_ctg and 
-      # at least 3 mate pair connections
-      n_ctg_counts[c] >= 3 and 
-      # at least 15 barcodes
-      len(n_ctg_bcodes[c] & bcode_set) >= 15 and
-      # only perform local asm in this bin if neighbor contig is
-      # lexicographically smaller
-      (c < root_ctg or c not in seed_ctgs)
-    ),
-    n_cand_ctgs,
-  )
-
-  local_asms = []
-  for n_ctg in n_ctgs:
-    assert n_ctg_counts[n_ctg] >= 3
-    n_bcode_counts = n_ctg_bcode_counts[n_ctg]
-    n_bcode_set = n_ctg_bcodes[n_ctg]
-    i_bcode_set = n_bcode_set & bcode_set
-    ds_bcode_set = i_bcode_set
-    # downsample to barcodes with the most reads
-    if len(i_bcode_set) > 400:
-      ds_bcode_set = set(sorted(
-        i_bcode_set,
-        reverse=True,
-        key=lambda(b): bcode_counts[b] + n_bcode_counts[b],
-      )[:400])
-    local_asms.append(
-      (n_ctg, i_bcode_set, ds_bcode_set)
+    ( _, _, 
+      link_ctg_counts,
+      link_ctg_qnames,
+      cov_bin_counts,
+    ) = get_ctg_links(root_ctg)
+  
+    # don't locally assemble any small contigs with out of whack coverage
+    if ctg_size_map[root_ctg] < SMALL_CTG_SIZE and is_whack_coverage(cov_bin_counts):
+      max_cov = max(cov_bin_counts.values())
+      med_cov = np.median(cov_bin_counts.values())
+      self.logger.log('max coverage {} order of magnitude higher than median {}'.format(
+        max_cov, med_cov))
+      return []
+  
+    link_cand_ctgs = filter(
+      lambda(c): (c != root_ctg and  link_ctg_counts[c] >= 3),
+      link_ctg_counts,
     )
 
-  # do local reassembly of the root contig
-  ds_bcode_set = bcode_set
-  if len(bcode_set) > 400:
-    ds_bcode_set = set(random.sample(bcode_set, 400))
+    self.logger.log('{} initial link candidates to examine'.format(
+      len(link_cand_ctgs)))
+    # examine recipricol links 
+    link_ctgs = []
+    # <ctg> : set(bcodes)
+    link_ctg_bcodes = {}
+    # <ctg> : Counter(bcodes)
+    link_ctg_bcode_counts = {}
+    for i, n_ctg in enumerate(link_cand_ctgs):
+      print 'n_ctg', n_ctg
+      (
+        link_ctg_bcodes[n_ctg],
+        link_ctg_bcode_counts[n_ctg],
+        _link_ctg_counts,
+        _link_ctg_qnames,
+        _cov_bin_counts,
+      ) = get_ctg_links(n_ctg)
+      # link is not recripricol
+      if _link_ctg_counts[root_ctg] < 3:
+        continue
+      # link contig has whacky coverage profile
+      # FIXME consider removing this, may not be necessary
+      if (
+        ctg_size_map[n_ctg] < SMALL_CTG_SIZE and
+        is_whack_coverage(_cov_bin_counts)
+      ):
+        continue
+      # not enough barcode overlap
+      if len(link_ctg_bcodes[n_ctg] & root_bcode_set) < 15:
+        continue
+      
+      print '  - added'
+      link_ctgs.append(n_ctg)
 
-  local_asms.append(
-    (root_ctg, bcode_set, ds_bcode_set)
-  )
+    self.logger.log('  - {} pass reciprocal filtering'.format(
+      len(link_ctgs)))
 
-  fhandle.close()
-  return local_asms
+    # only return assemblies that aren't going to be duplicated in other
+    # bins
+    n_ctgs = filter(
+      lambda(c): (
+        c != root_ctg and 
+        # only perform local asm in this bin if neighbor contig is
+        # lexicographically smaller
+        (c < root_ctg or c not in seed_ctgs)
+      ),
+      link_ctgs,
+    )
+    ## FIXME remove
+    #n_ctgs = link_ctgs
 
+    local_asms = []
+    for n_ctg in n_ctgs:
+      assert link_ctg_counts[n_ctg] >= 3
+      link_bcode_counts = link_ctg_bcode_counts[n_ctg]
+      i_bcode_set = link_ctg_bcodes[n_ctg] & root_bcode_set
+      ds_bcode_set = i_bcode_set
+      # downsample to barcodes with the most reads
+      if len(i_bcode_set) > 400:
+        ds_bcode_set = set(sorted(
+          i_bcode_set,
+          reverse=True,
+          key=lambda(b): bcode_counts[b] + link_bcode_counts[b],
+        )[:400])
+      local_asms.append(
+        (n_ctg, i_bcode_set, ds_bcode_set)
+      )
+
+    # do local reassembly of the root contig
+    ds_bcode_set = root_bcode_set
+    if len(root_bcode_set) > 400:
+      ds_bcode_set = set(random.sample(root_bcode_set, 400))
+  
+    local_asms.append(
+      (root_ctg, root_bcode_set, ds_bcode_set)
+    )
+  
+    return local_asms
+  
+    # DEBUG
+    #--------------------------------------------------------------------------
+    # for each local assembly, dump the paired end reads
+    ctgs = list(n_ctgs)
+    ctgs.append(root_ctg)
+  
+    fouts_map = {}
+    nctg_fout_map = {}
+    paths = []
+    for n_ctg in n_ctgs:
+      path = os.path.join(self.outdir, '{}.bam'.format(n_ctg))
+      paths.append(path)
+      nctg_fout_map[n_ctg] = pysam.Samfile(path, 'wb', template=fhandle)
+    path = os.path.join(self.outdir, 'all.bam')
+    paths.append(path)
+    nctg_fout_map['all'] = pysam.Samfile(path, 'wb', template=fhandle)
+  
+    qname_nctgs_map = defaultdict(set)
+    for n_ctg, qname_set in link_ctg_qnames.items():
+      if n_ctg == root_ctg or n_ctg not in n_ctgs:
+        continue
+      for qname in qname_set:
+        qname_nctgs_map[qname].add(n_ctg)
+  
+    for ctg in ctgs:
+      for read in fhandle.fetch(ctg):
+        if read.qname not in qname_nctgs_map:
+          continue
+        for n_ctg in qname_nctgs_map[read.qname]:
+          # FIXME remove
+          if n_ctg not in nctg_fout_map:
+            continue
+          nctg_fout_map[n_ctg].write(read)
+        nctg_fout_map['all'].write(read)
+  
+    # close
+    fhandle.close()
+    for fout_handle in nctg_fout_map.values():
+      fout_handle.close()
+  
+    # do local reassembly of the root contig
+    ds_bcode_set = root_bcode_set
+    if len(root_bcode_set) > 400:
+      ds_bcode_set = set(random.sample(root_bcode_set, 400))
+  
+    # FIXME TODO
+    # for large contigs only locally reassemble the end 10kb
+    local_asms.append(
+      (root_ctg, root_bcode_set, ds_bcode_set)
+    )
+  
+    return local_asms
+  
 def get_lrhints_fa(
   ctgs,
   infa_path,
@@ -350,4 +482,4 @@ def get_bcode_reads(infq_paths, outfa_path, bcode_set):
 #    rclip = (c in [4,5])
 #  
 #    return (lclip, rclip)
-
+  
